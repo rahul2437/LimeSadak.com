93023759c631bd3ced5b94efeb039455ffe54734
Rohitsharma7877 <rohitsharma.sharma194@gmail.com>
2023-01-19 11:30:13 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
       {/* <AllRoutes /> */}
       {/* <VenderList/> */}
       {/* <UserList/> */}
-      <ProductList/>
+      {/* <ProductList/> */}
     </div>
   );
 }
--- frontend/src/components/adminSidebar/AdminSidebar.jsx
+++ frontend/src/components/adminSidebar/AdminSidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from './adminSidebar.module.css'
+import style from './AdminSidebar.module.css'
 import {Link} from 'react-router-dom';
 
 const AdminSidebar = () => {
