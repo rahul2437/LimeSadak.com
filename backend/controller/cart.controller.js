4449ef19588039ef255a454a9cde27afcbb01c47
Rahul Sheelavantar <rahulsheelavantar@gmail.com>
2023-01-19 23:16:19 +0530

    Cart populate

--- backend/controller/cart.controller.js
+++ backend/controller/cart.controller.js
@@ -1,106 +1,54 @@
 const { Cart } = require("../models/cart.model");
-
+const { Product } = require("../models/product.model");
 exports.addToCart = async (req, res) => {
-  Cart.findOne({ user: req.user._id }).exec((err, cart) => {
-    if (err) return res.send({ err });
-    if (!cart) {
-      const _cart = new Cart({
-        user: req.user._id,
-        cartItems: [req.body.cartItems],
-      });
-      _cart.save((e, data) => {
-        if (e)
-          return res
-            .status(400)
-            .send({ message: "Something went wrong", error: e });
-        if (data) {
-          return res.status(200).send({ message: "Added to cart", data });
-        } else {
-          return res.status(200).send({ message: "Not added to cart" });
-        }
-      });
-    } else {
-      const product = req.body.cartItems.product;
-      const item = cart.cartItems.find((ele) => ele.product == product);
-      if (item) {
-        return res.send({ message: "cart present", item });
+  const { cartItems } = req.body;
+  const user = req.user._id;
+  const payload = { user: req.user._id, cartItems: [cartItems] };
+  const { product } = cartItems;
+  try {
+    const isUserInCart = await Cart.findOne({ user });
+    if (isUserInCart) {
+      let updateCart = isUserInCart.cartItems.find(
+        (item) => item.product == product
+      );
+      if (updateCart) {
+        let newCart = await Cart.findOneAndUpdate(
+          {
+            user,
+            "cartItems.product": product,
+          },
+          {
+            cartItems,
+          }
+        );
+        return res.send({ message: "Product in cart updated", newCart });
       } else {
-        Cart.findOneAndUpdate(
-          { user: req.user._id },
+        let newCart = await Cart.findOneAndUpdate(
+          { user },
           {
             $push: {
-              cartItems: req.body.cartItems,
+              cartItems,
             },
           }
-        ).exec((e, data) => {
-          if (e)
-            return res
-              .status(400)
-              .send({ message: "Something went wrong", error: e });
-          if (data) {
-            return res.status(200).send({ message: "Added", cart: data });
-          }
-        });
+        );
+        return res.send({ message: "Added Product to cart ", newCart });
       }
+    } else {
+      let newCart = new Cart(payload);
+      await newCart.save();
+      return res.send({ message: "Added item to cart", newCart });
     }
-  });
+  } catch (error) {
+    return res.send({ message: "Something went wrong", error: error.message });
+  }
 };
 
-exports.addToCart1 = (req, res) => {
-  Cart.findOne({ user: req.user._id }).exec((err, cart) => {
-    if (err) return res.status(400).send({ message: "Something went wrong" });
-    if (cart) {
-      const product = req.body.cartItems.product;
-      const item = cart.cartItems.find((c) => c.product == product);
-      if (item) {
-        Cart.findByIdAndUpdate(
-          { user: req.user._id, "cartItems.product": product },
-          {
-            $set: {
-              ...req.body.cartItems,
-              quantity: item.quantity + req.body.cartItems.quantity,
-            },
-          }
-        ).exec((err, _cart) => {
-          if (err)
-            return res
-              .status(400)
-              .send({ message: "Something went wrong", error: err });
-          if (_cart) {
-            return res.status(200).send({ message: "Added", cart: _cart });
-          }
-        });
-      } else {
-        Cart.findByIdAndUpdate(
-          { user: req.user._id },
-          {
-            $push: {
-              cartItems: req.body.cartItems,
-            },
-          }
-        ).exec((err, _cart) => {
-          if (err)
-            return res
-              .status(400)
-              .send({ message: "Something went wrong", error: err });
-          if (_cart) {
-            return res.status(200).send({ message: "Added", cart: _cart });
-          }
-        });
-      }
-    } else {
-      const cart = new Cart({
-        user: req.user._id,
-        cartItems: [req.body.cartItems],
-      });
-
-      cart.save((err, cart) => {
-        if (err)
-          return res.status(400).send({ message: "Something went wrong", err });
-        if (cart) {
-          return res.status(200).send({ message: "Added", cart });
-        }
-      });
-    }
-  });
+exports.getCartItems = async (req, res) => {
+  const user = req.user._id;
+  try {
+    let cart = await Cart.findOne({ user }).populate("cartItems.product");
+    return res.send({ message: "Cart Items", cart });
+  } catch (error) {
+    return res.send({ message: "Something went wrong", error: error.message });
+  }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+require("./models/product.model");
 
 const { connection } = require("./config/db");
 const { userRouter } = require("./routes/user.route");
 const { productRouter } = require("./routes/product.route");
 const { adminRouter } = require("./routes/admin/admin.route");
 const { cartRouter } = require("./routes/cart.route");
-
 const app = express();
 app.use(express.json());
 app.use(
