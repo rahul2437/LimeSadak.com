99b7840b0da66b6d5f7b3508cb2845ef6a08febe
Rahul Sheelavantar <rahulsheelavantar@gmail.com>
2023-01-20 15:07:19 +0530

    Testing deployment 1

--- backend/index.js
+++ backend/index.js
@@ -18,18 +18,77 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("./index.html", (err, data) => {
-    if (err) {
-      res.write(
-        "Welcome to Limesadak api, Sorry server error cannot show Usage now"
-      );
-      res.end();
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
+  res.send(`
+      <div id="main">
+      <h1>Welcome to Limesadak API</h1>
+    </div>
+    <div>
+      <div>Usage</div>
+      <h3>
+        Deployed on Vercel : <span>https://general-icicle-9828.vercel.app</span>
+      </h3>
+      <ul>
+        <li>
+          Signin :
+          <span>https://general-icicle-9828.vercel.app/user/signin</span>
+          Needed:
+          <code>
+            {<br />
+            "name": "String",<br />
+            "email": "String",<br />
+            "password": "String",<br />
+            "phone_number": "String",<br />
+            "role": [Array of string],<br />
+            }
+          </code>
+        </li>
+        <li>
+          Signup :
+          <span>https://general-icicle-9828.vercel.app/user/signup</span>
+          Needed:
+          <code>
+            {<br />
+            "email": "String",<br />
+            "password": "String",<br />
+            }
+          </code>
+        </li>
+        <li>
+          Get all Products:
+          <span>https://general-icicle-9828.vercel.app/products</span>
+        </li>
+        <li>
+          Add products needed fields :
+          <span> https://general-icicle-9828.vercel.app/products/add </span>
+          <code>
+            <br />
+            { "name": "String",<br />
+            "brand": "String",<br />
+            "description": "String",<br />
+            "category": [Array of string],<br />
+            "color": "String",<br />
+            "gender": "String",<br />
+            "tags": [Array of string],<br />
+            "size": [Array of string],<br />"stock": Number,<br />
+            "price": Number,<br />
+            }
+          </code>
+        </li>
+        <li>
+          Update product,delete product,single product:
+          <span>https://general-icicle-9828.vercel.app/product/:id</span>
+        </li>
+        <li>
+          Get cart items:
+          <span> https://general-icicle-9828.vercel.app/cart </span>
+        </li>
+        <li>
+          Add to cart:
+          <span> https://general-icicle-9828.vercel.app/cart/addtocart </span>
+        </li>
+      </ul>
+    </div>
+  `);
 });
 
 app.use("/user", userRouter);
