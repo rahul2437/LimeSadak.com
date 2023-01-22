5b1d2fb446bbe724773d787379aed0ae17fbffed
Sourav Kumar <kmrsourav786@gmail.com>
2023-01-22 21:53:56 +0530

    fixed navabar & added resp. & signin-signUp

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,25 +2,80 @@ import React from 'react'
 import style from './Navbar.module.css'
 import {Link} from 'react-router-dom'
 import logo from "../../../src/assets/limesadak.jpg"
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button,
+  IconButton
+} from '@chakra-ui/react'
+
+import { ChevronDownIcon, HamburgerIcon } from '@chakra-ui/icons'
 
 const Navbar = () => {
   return (
     <div className={style.navbar}>
+      <div className={style.ham}>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label='Options'
+            icon={<HamburgerIcon />}
+            variant='outline'
+          />
+          <MenuList>
+            <MenuItem>
+              <Link to="/products?gender=female"><button>WOMEN</button></Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/products?gender=male"><button>MEN</button></Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/products?gender=kids"><button>KIDS</button></Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/"><button>HOME</button></Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/offer"><button>OFFER</button></Link>
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
       <div className={style.left}>
-        <img src={logo} alt="" />
-        <ul>
-          <li><Link to="/">WOMEN</Link></li>
-          <li><Link to="/">MEN</Link></li>
-          <li><Link to="/">KIDS</Link></li>
-          <li><Link to="/">HOME</Link></li>
-          <li><Link to="/" className={style.offer}>OFFERS</Link></li>
-        </ul>
+        <Link to="/"><img src={logo} alt="" /></Link>
+        <div className={style.menu}>
+          <ul >
+            <li><Link to="/products?gender=female">WOMEN</Link></li>
+            <li><Link to="/products?gender=male">MEN</Link></li>
+            <li><Link to="/products?gender=kids">KIDS</Link></li>
+            <li><Link to="/">HOME</Link></li>
+            <li><Link to="/offer" className={style.offer}>OFFERS</Link></li>
+          </ul>
+        </div>
       </div>
       <div className={style.right}>
         <Link>SCRAPBOOK</Link>
         <Link>SEARCH</Link>
         <Link>CART</Link>
-        <Link>PROFILE</Link>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            PROFILE
+          </MenuButton>
+          <MenuList>
+            <MenuItem>
+              <Link to="/signin"><button>Sign In</button></Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/signup"><button>Sign Up</button></Link>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </div>
     </div>
   )
--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { signup } from "../../redux/AuthReducer/action";
-import AdminNavbar from "../adminNavbar/AdminNavbar";
-import "./Signup.css";
+import "../signin/Signin.css";
 
 const initForm = {
   name: "",
@@ -27,49 +26,57 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <AdminNavbar />
-      <h1>SIGN-UP</h1>
-      <form id="signup-form" onSubmit={onSubmit}>
-        <label>Name</label>
-        <input
-          onChange={(e) => onChange(e)}
-          placeholder="Enter Name"
-          type="text"
-          name="name"
-          required
-        />
-        <label>Phone Number</label>
-        <input
-          onChange={(e) => onChange(e)}
-          placeholder="Enter Phone number"
-          type="text"
-          name="phone_number"
-          required
-        />
-        <label>Role</label>
-        <select onChange={(e) => onChange(e)} name="role">
-          <option value="user">customer</option>
-          <option value="vendor">vendor</option>
-        </select>
-        <label>Email</label>
-        <input
-          onChange={(e) => onChange(e)}
-          placeholder="Enter Email"
-          type="text"
-          name="email"
-          required
-        />
-        <label>Password</label>
-        <input
-          onChange={(e) => onChange(e)}
-          placeholder="Enter Password"
-          type="password"
-          name="password"
-          required
-        />
-        <input type="submit" value="SIGNUP" id="signup" />
-      </form>
+    <div className="container">
+
+      {/* TOP */}
+
+      <div className="top">
+        <img src="https://img1.junaroad.com/assets/images/mobileNotif/1526210608201.jpg" alt="" />
+        <div>
+          <img src="https://img1.junaroad.com/assets/images/mobileNotif/1525951378657.jpg" alt="" /><span>SIGN UP</span>
+          <p>sign up to proceed further</p>
+        </div>
+      </div>
+
+      {/* BOTTOM */}
+
+      <div className="bottom">
+        <form id="signup-form" onSubmit={onSubmit}>
+          <input
+            onChange={(e) => onChange(e)}
+            placeholder="Enter Name"
+            type="text"
+            name="name"
+            required
+          />
+          <input
+            onChange={(e) => onChange(e)}
+            placeholder="Enter Phone number"
+            type="text"
+            name="phone_number"
+            required
+          />
+          <select onChange={(e) => onChange(e)} name="role">
+            <option value="user">customer</option>
+            <option value="vendor">vendor</option>
+          </select>
+          <input
+            onChange={(e) => onChange(e)}
+            placeholder="Enter Email"
+            type="text"
+            name="email"
+            required
+          />
+          <input
+            onChange={(e) => onChange(e)}
+            placeholder="Enter Password"
+            type="password"
+            name="password"
+            required
+          />
+          <input type="submit" value="SIGNUP" id="signup" />
+        </form>
+      </div>
     </div>
   );
 };
